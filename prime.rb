3559ecc1ddbbb89b3ee30948653bc3adfca652be
Dario Carlino <baires82@gmail.com>
2020-08-23 03:34:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer >= 2
+    array = (2..(integer - 1)).to_a
+    array.each do |number|
+      if integer % number == 0
+        return false
+      end
+    end
+  elsif integer <= 0 || integer == 1
+    return false
+  end
+  true
+end
