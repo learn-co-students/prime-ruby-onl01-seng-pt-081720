0059dd784132d945c0e2b115b3c537a1da763fb3
Lisa Walters <campi_l@fortlewis.edu>
2020-08-27 01:44:16 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+
+def prime?(num)
+
+  if
+  num <= 1
+  return false
+
+  elsif
+
+    num == 2
+     return true
+
+  else
+
+i = 2
+
+arr = []
+while i < num
+answer = num % i
+arr.push(answer)
+i += 1
+end
+
+result = arr.any? {|number| number == 0}
+return !result
+
+end
+end
