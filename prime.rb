bd3730a4569564dc660de85c117da716cd4bf155
Ken Barlow <kenbrlow@gmail.com>
2020-08-28 23:12:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+
+  if number <= 1
+    return false
+  end
+
+  n = 2
+  while n < number
+    return false if number % n == 0
+    n += 1
+  end
+  true
+end
