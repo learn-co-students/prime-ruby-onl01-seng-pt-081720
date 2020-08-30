62d9b91e3c2afbf190aa6e8560a2bfde9fb0b3cf
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-30 05:40:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
-def prime?(num)
-  if num <= 1 
+def prime?(integer)
+  if integer
     return false
   else
-    (2..num - 1).each { |int| return false if num % int ==0}
+    (2..integer - 1).each { |num| return false if integer % num ==0}
   end
   true
 end
