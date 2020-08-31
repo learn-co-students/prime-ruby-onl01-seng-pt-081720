b914017f1a4b59b806d490c607fe6ec18431830b
Myles Janik <mylesjanik@gmail.com>
2020-08-31 02:07:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
 # Add  code here!
 
 def prime?(int)
-  
+  if int <= 1
+    return false
+  else
+    if (2...n) .any? { |i| int % i == 0}
+      return false
+    else
+      return true
+    end
 end
