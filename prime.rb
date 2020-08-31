076b4423d458cefe173afcb42d131c46bb17fcdd
Myles Janik <mylesjanik@gmail.com>
2020-08-31 01:52:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 # Add  code here!
 
 def prime?(int)
-if
-	i=2
-	while(i<num)
-		if (num%i==0)
-			count+=1
-		end
-		i+=1
-	end
+  counter = 0
+  if
+  	i=2
+  	while(i<num)
+  		if (num%i==0)
+  			counter+=1
+  		end
+  		i+=1
+  	end
 	
-end
+  end
 end
