e984806c06012b72ece9953579b84446c015dfa0
Myles Janik <mylesjanik@gmail.com>
2020-08-31 01:56:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,5 @@
 # Add  code here!
 
 def prime?(int)
-  counter = 0
-  num = 2
-  while(num<int)
-  	if (int%num==0)
-  		counter+=1
-  	end
-  	num+=1
-	end
-	
-	if counter >= 1
-	  return false
-	else
-	  return true
-	end
+  
 end
